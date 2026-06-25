ca7f02f28ae3458d895c21408364956771cc8afc
Raj <rajgour617@gmail.com>
2026-06-25 22:53:29 +0530

    01-react-beginning added

---
+++ 01-react-beginning/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1> Hello, this is Raj! </h1>
+      </div>
+      <h3> this is my first react project </h3>
+    </>
+  )
+}
+
+export default App
